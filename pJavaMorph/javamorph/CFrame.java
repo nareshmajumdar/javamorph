ff25f693741746b0cda13af33b10b0806505893c
claus.erhard.wimmer <claus.erhard.wimmer@ed91e616-ddc1-11dd-bd7b-b12b6590754f>
2010-02-01 14:21:00 +0000

    Copyright information added.

--- pJavaMorph/javamorph/CFrame.java
+++ pJavaMorph/javamorph/CFrame.java
@@ -7,11 +7,29 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @version 1.1
+ * File belongs to javamorph (Merging of human-face-pictures).
+ * Copyright (C) 2009 - 2010  Claus Wimmer
+ * See file ".../help/COPYING" for details!
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA * 
+ *
+ * @version 1.5
  * <br/>
  * @author claus.erhard.wimmer@googlemail.com
  * <br/>
- * Program: JavaMorph V 1.1.
+ * Program: JavaMorph.
  * <br/>
  * Class: CFrame.
  * <br/>
@@ -45,15 +63,15 @@ public class CFrame extends JLabel {
     /** Double buffer image to avoid flickering effects. */
     private BufferedImage moffline;  
     /** Picture's mesh. */
-    private Vector<Point> mesh;
+    protected Vector<Point> mesh;
     /** Picture's clip polygon points. */
-    private Vector<Point> polygon;
+    protected Vector<Point> polygon;
     /** Picture itself. */
     private BufferedImage image;
     /** Polygon's smoothed clip matrix. */
-    private double clip[][];
+    protected double clip[][];
     /** File to store a debug copy of the clip matrix. */
-    private File f_clip;
+    protected File f_clip;
     /** 
      * Constructor.
      * 
